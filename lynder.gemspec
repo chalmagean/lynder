f972073c9c77054ff508fbb2601eab6348b02d98
Cezar Halmagean <cezar@mixandgo.com>
2016-08-10 16:18:39 +0300

    Reorder files in folder

--- lynder.gemspec
+++ lynder.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Cezar Halmagean"]
   spec.email         = ["cezar@mixandgo.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{}
+  spec.description   = %q{}
+  spec.homepage      = ""
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -30,4 +30,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "fakefs", "~> 0.9"
 end
--- spec/lynder_spec.rb
+++ spec/lynder_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
+# We need this for compatibility reasons between Ruby 2.3 and FakeFS
+require 'pp'
+
 describe Lynder do
+  include FakeFS::SpecHelpers
+
   it 'has a version number' do
     expect(Lynder::VERSION).not_to be nil
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    files = [
+      "file-name-431622.mp4",
+      "file-name-431615.mp4",
+      "file-name-431614.mp4",
+      "file-name-431616.mp4",
+      "file-name-431628.mp4"
+    ]
+
+    FileUtils.touch(files)
+    Lynder::Rename.reorder!
+
+    expected = [
+      "001. file-name-431614.mp4",
+      "002. file-name-431615.mp4",
+      "003. file-name-431616.mp4",
+      "004. file-name-431622.mp4",
+      "005. file-name-431628.mp4"
+    ]
+
+    files_in_current_dir = Dir.glob('*').map { |f| f.gsub(/^\//, '') }
+    expect(files_in_current_dir).to eq(expected)
   end
 end
